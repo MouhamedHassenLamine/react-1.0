54e70e018790c45f85e44771e88d1e6027ab4441
MouhamedHassenLamine <121714683+MouhamedHassenLamine@users.noreply.github.com>
2023-04-04 17:44:57 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Nav_bar from './component/Nav_bar';
+import Formm from './component/Form';
+import Contact from './component/Contact';
+
+function App() {
+  return (<> 
+  <Nav_bar></Nav_bar>
+  <Contact></Contact>
+ <Formm></Formm></>
+  );
+  
+}
+
+export default App;
---
+++ src/component/Contact.jsx
@@ -0,0 +1,12 @@
+function Contact(){
+ return (
+    <div className="cont">
+    <h1>
+      Contact   US
+    </h1>  
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur laboriosam,<br></br> magnam eos omnis architecto ipsum commodi nostrum perspiciatis iste deleniti<br></br> ullam, ducimus impedit nam numquam aliquid quis ex voluptatum voluptates!<br></br>
+    Ducimus in accusamus laudantium quisquam voluptates temporibus saepe error,<br></br> repellat quam debitis reiciendis rerum, velit facere quos libero nisi ratione. <br></br>Recusandae ad tenetur in culpa eligendi non quis officiis doloremque.</p>
+      </div>
+ )
+}
+export default Contact
